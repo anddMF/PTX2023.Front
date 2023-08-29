c0fe8f14033674858ba9a3402531dc30357ea0cd
Andrew Moraes <andrewmoraesds@gmail.com>
2023-08-29 17:24:32 -0400

    add css classes for each weather

--- angular-front/ptx-front/src/app/shared/services/weather-class.service.ts
+++ angular-front/ptx-front/src/app/shared/services/weather-class.service.ts
@@ -10,19 +10,16 @@ export class WeatherClassService {
 
   getClassification(weatherCode: number) {
     switch (weatherCode) {
-      case 1 || 30:
+      case 1: case 30:
         return WeatherClassEnum.sunny;
 
       case 2:
         return WeatherClassEnum.mostlysunny;
 
-      case 3 || 4:
+      case 3: case 4:
         return WeatherClassEnum.partlysunny;
 
-      case 5 || 6:
-        return WeatherClassEnum.mostlysunny;
-
-      case 7 || 8 || 38:
+      case 7: case 8: case 38: case 5: case 6:
         return WeatherClassEnum.cloudy;
 
       case 11:
@@ -31,22 +28,22 @@ export class WeatherClassService {
       case 12:
         return WeatherClassEnum.showersday;
 
-      case 13 || 14:
+      case 13: case 14:
         return WeatherClassEnum.sunnyshowers;
 
-      case 15 || 16 || 17:
+      case 15: case 16: case 17:
         return WeatherClassEnum.tstormday;
 
       case 18:
         return WeatherClassEnum.rainday;
 
-      case 19 || 20 || 21 || 43:
+      case 19: case 20: case 21: case 43:
         return WeatherClassEnum.flurriesday;
 
-      case 22 || 23 || 29 || 31 || 44:
+      case 22: case 23: case 29: case 31: case 44:
         return WeatherClassEnum.snow;
 
-      case 24 || 25 || 26:
+      case 24: case 25: case 26:
         return WeatherClassEnum.freezingrain;
 
       case 32:
@@ -58,13 +55,13 @@ export class WeatherClassService {
       case 34:
         return WeatherClassEnum.mostlyclear;
 
-      case 35 || 36 || 37:
+      case 35: case 36: case 37:
         return WeatherClassEnum.partlyclear;
 
-      case 39 || 40:
+      case 39: case 40:
         return WeatherClassEnum.showersnight;
 
-      case 41 || 42:
+      case 41: case 42:
         return WeatherClassEnum.tstormnight;
 
       default: {
